1a08436c5e242e84c8973f772e01011f9568b1ca
Yunus <aliyunus291@gmail.com>
2020-03-05 10:59:32 +0300

    path location changes

--- recognition/faceRecognition.py
+++ recognition/faceRecognition.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def faceDetection(test_img):
     gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-    face_haar_cascade = cv2.CascadeClassifier('/home/darknet/PycharmProjects/Python/recognition/HaarCascade/haarcascade_frontalface_default.xml')
+    face_haar_cascade = cv2.CascadeClassifier('..\HaarCascade\haarcascade_frontalface_default.xml')
     faces = face_haar_cascade.detectMultiScale(gray_img, scaleFactor=1.2, minNeighbors=3)
     return faces, gray_img
 
