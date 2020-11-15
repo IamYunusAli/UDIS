bb75be780e11631dcc4fc94dc825c8691dfc998b
Yunus Ali <aliyunus291@gmail.com>
2020-11-15 20:40:09 +0300

    user interface minimal changes
    Signed-off-by: Yunus Ali <aliyunus291@gmail.com>

--- ui/ui_main.py
+++ ui/ui_main.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
@@ -19,35 +29,127 @@ class Ui_MainWindow(object):
         self.mainframe.setObjectName(u"mainframe")
         self.mainframe.setFrameShape(QFrame.StyledPanel)
         self.mainframe.setFrameShadow(QFrame.Raised)
-        self.screen = QLabel(self.mainframe)
-        self.screen.setObjectName(u"screen")
-        self.screen.setGeometry(QRect(-10, -10, 800, 600))
-        font = QFont()
-        font.setFamily(u"Segoe UI Semibold")
-        font.setPointSize(25)
-        font.setBold(True)
-        font.setWeight(75)
-        self.screen.setFont(font)
-        self.screen.setStyleSheet(u"QFrame {	\n"
-"	background-color: rgb(56, 58, 89);	\n"
-"	color: rgb(220, 220, 220);\n"
-"}")
-        self.screen.setAlignment(Qt.AlignCenter)
         self.pushButton = QPushButton(self.mainframe)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(50, 530, 131, 41))
-        font1 = QFont()
-        font1.setFamily(u"Segoe UI Semibold")
-        font1.setPointSize(19)
-        font1.setBold(True)
-        font1.setWeight(75)
-        self.pushButton.setFont(font1)
-        self.pushButton.setStyleSheet(u"background-color: rgba(93, 179, 100);")
+        self.pushButton.setGeometry(QRect(50, 540, 131, 31))
+        font = QFont()
+        font.setFamily(u"Segoe UI Semibold")
+        font.setPointSize(15)
+        font.setBold(True)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        self.pushButton.setFont(font)
+        self.pushButton.setStyleSheet(u"background-color: rgba(93, 179, 100);\n"
+"shadow: rgb(220,230,221)")
         self.pushButton_2 = QPushButton(self.mainframe)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(250, 530, 121, 41))
+        self.pushButton_2.setGeometry(QRect(250, 540, 131, 31))
+        font1 = QFont()
+        font1.setFamily(u"Segoe UI Semibold")
+        font1.setPointSize(15)
+        font1.setBold(True)
+        font1.setWeight(75)
         self.pushButton_2.setFont(font1)
         self.pushButton_2.setStyleSheet(u"background-color: rgba(163, 65, 65);")
+        self.pushButton_2.setFlat(False)
+        self.videowindow = QFrame(self.mainframe)
+        self.videowindow.setObjectName(u"videowindow")
+        self.videowindow.setGeometry(QRect(10, 40, 581, 471))
+        self.videowindow.setStyleSheet(u"background-color: rgb(52, 59, 72\n"
+"\n"
+");\n"
+"")
+        self.videowindow.setFrameShape(QFrame.StyledPanel)
+        self.videowindow.setFrameShadow(QFrame.Raised)
+        self.caughtpic = QFrame(self.mainframe)
+        self.caughtpic.setObjectName(u"caughtpic")
+        self.caughtpic.setGeometry(QRect(600, 40, 171, 191))
+        self.caughtpic.setStyleSheet(u"background-color: rgb(52, 59, 72);")
+        self.caughtpic.setFrameShape(QFrame.StyledPanel)
+        self.caughtpic.setFrameShadow(QFrame.Raised)
+        self.caughtlicense = QFrame(self.mainframe)
+        self.caughtlicense.setObjectName(u"caughtlicense")
+        self.caughtlicense.setGeometry(QRect(600, 260, 171, 41))
+        self.caughtlicense.setStyleSheet(u"background-color: rgb(52, 59, 72);")
+        self.caughtlicense.setFrameShape(QFrame.StyledPanel)
+        self.caughtlicense.setFrameShadow(QFrame.Raised)
+        self.titlebar = QFrame(self.mainframe)
+        self.titlebar.setObjectName(u"titlebar")
+        self.titlebar.setGeometry(QRect(0, 0, 431, 31))
+        self.titlebar.setFrameShape(QFrame.StyledPanel)
+        self.titlebar.setFrameShadow(QFrame.Raised)
+        self.label = QLabel(self.titlebar)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, -5, 71, 31))
+        font2 = QFont()
+        font2.setFamily(u"A0 Addis Abeba Unicode")
+        font2.setPointSize(18)
+        font2.setBold(True)
+        font2.setWeight(75)
+        self.label.setFont(font2)
+        self.btn_close = QPushButton(self.mainframe)
+        self.btn_close.setObjectName(u"btn_close")
+        self.btn_close.setGeometry(QRect(730, -10, 40, 42))
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.btn_close.sizePolicy().hasHeightForWidth())
+        self.btn_close.setSizePolicy(sizePolicy)
+        self.btn_close.setMinimumSize(QSize(40, 0))
+        self.btn_close.setMaximumSize(QSize(40, 16777215))
+        self.btn_close.setStyleSheet(u"QPushButton {	\n"
+"	border: none;\n"
+"	background-color: transparent;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: rgb(52, 59, 72);\n"
+"}\n"
+"QPushButton:pressed {	\n"
+"	background-color: rgb(85, 170, 255);\n"
+"}")
+        icon = QIcon()
+        icon.addFile(u":/16x16/icons/16x16/cil-x.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_close.setIcon(icon)
+        self.btn_maximize_restore = QPushButton(self.mainframe)
+        self.btn_maximize_restore.setObjectName(u"btn_maximize_restore")
+        self.btn_maximize_restore.setGeometry(QRect(690, -10, 40, 42))
+        sizePolicy.setHeightForWidth(self.btn_maximize_restore.sizePolicy().hasHeightForWidth())
+        self.btn_maximize_restore.setSizePolicy(sizePolicy)
+        self.btn_maximize_restore.setMinimumSize(QSize(40, 0))
+        self.btn_maximize_restore.setMaximumSize(QSize(40, 16777215))
+        self.btn_maximize_restore.setStyleSheet(u"QPushButton {	\n"
+"	border: none;\n"
+"	background-color: transparent;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: rgb(52, 59, 72);\n"
+"}\n"
+"QPushButton:pressed {	\n"
+"	background-color: rgb(85, 170, 255);\n"
+"}")
+        icon1 = QIcon()
+        icon1.addFile(u":/16x16/icons/16x16/cil-window-maximize.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_maximize_restore.setIcon(icon1)
+        self.btn_minimize = QPushButton(self.mainframe)
+        self.btn_minimize.setObjectName(u"btn_minimize")
+        self.btn_minimize.setGeometry(QRect(650, -10, 40, 42))
+        sizePolicy.setHeightForWidth(self.btn_minimize.sizePolicy().hasHeightForWidth())
+        self.btn_minimize.setSizePolicy(sizePolicy)
+        self.btn_minimize.setMinimumSize(QSize(40, 0))
+        self.btn_minimize.setMaximumSize(QSize(40, 16777215))
+        self.btn_minimize.setStyleSheet(u"QPushButton {	\n"
+"	border: none;\n"
+"	background-color: transparent;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: rgb(52, 59, 72);\n"
+"}\n"
+"QPushButton:pressed {	\n"
+"	background-color: rgb(85, 170, 255);\n"
+"}")
+        icon2 = QIcon()
+        icon2.addFile(u":/16x16/icons/16x16/cil-window-minimize.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.btn_minimize.setIcon(icon2)
 
         self.verticalLayout.addWidget(self.mainframe)
 
@@ -60,8 +162,20 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.screen.setText(QCoreApplication.translate("MainWindow", u"UDIS", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"START", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"STOP", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"UDIS", None))
+#if QT_CONFIG(tooltip)
+        self.btn_close.setToolTip(QCoreApplication.translate("MainWindow", u"Close", None))
+#endif // QT_CONFIG(tooltip)
+        self.btn_close.setText("")
+#if QT_CONFIG(tooltip)
+        self.btn_maximize_restore.setToolTip(QCoreApplication.translate("MainWindow", u"Maximize", None))
+#endif // QT_CONFIG(tooltip)
+        self.btn_maximize_restore.setText("")
+#if QT_CONFIG(tooltip)
+        self.btn_minimize.setToolTip(QCoreApplication.translate("MainWindow", u"Minimize", None))
+#endif // QT_CONFIG(tooltip)
+        self.btn_minimize.setText("")
     # retranslateUi
 
